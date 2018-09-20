3b0c5edde425cfb65f3e70bdb3be474530e30bba
Ualter Azambuja <ualter.junior@gmail.com>
2018-09-20 23:16:25 +0200

    .

---
+++ src/main/java/ujr/websocket/server/ChatEndpoint.java
@@ -0,0 +1,85 @@
+package ujr.websocket.server;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import ujr.websocket.model.Message;
+
+@ServerEndpoint(value = "/chat/{username}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
+public class ChatEndpoint {
+
+	private Session session;
+	private static Set<ChatEndpoint> chatEndpoints = new CopyOnWriteArraySet<>();
+	private static HashMap<String, String> users = new HashMap<>();
+
+	@OnOpen
+	public void onOpen(Session session, @PathParam("username") String username) throws IOException {
+		this.session = session;
+		chatEndpoints.add(this);
+		users.put(session.getId(), username);
+
+		Message message = new Message();
+		message.setFrom(username);
+		message.setContent("Connected!");
+		
+		try {
+			broadcast(message);
+		} catch (EncodeException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@OnMessage
+	public void onMessage(Session session, Message message) throws IOException {
+		message.setFrom(users.get(session.getId()));
+		
+		try {
+			broadcast(message);
+		} catch (EncodeException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@OnClose
+	public void onClose(Session session) throws IOException {
+		chatEndpoints.remove(this);
+        Message message = new Message();
+        message.setFrom(users.get(session.getId()));
+        message.setContent("Disconnected!");
+        
+        try {
+			broadcast(message);
+		} catch (EncodeException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		// Do error handling here
+	}
+
+	private static void broadcast(Message message) throws IOException, EncodeException {
+		chatEndpoints.forEach(endpoint -> {
+			synchronized (endpoint) {
+				try {
+					endpoint.session.getBasicRemote().sendObject(message);
+				} catch (IOException | EncodeException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
---
+++ src/main/java/ujr/websocket/server/MessageEncoder.java
@@ -0,0 +1,30 @@
+package ujr.websocket.server;
+
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+import com.google.gson.Gson;
+
+import ujr.websocket.model.Message;
+
+public class MessageEncoder implements Encoder.Text<Message> {
+
+	private static Gson gson = new Gson();
+
+	@Override
+	public String encode(Message message) throws EncodeException {
+		String json = gson.toJson(message);
+		return json;
+	}
+
+	@Override
+	public void init(EndpointConfig endpointConfig) {
+		// Custom initialization logic
+	}
+
+	@Override
+	public void destroy() {
+		// Close resources
+	}
+}
